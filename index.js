7ab87b0c355572ef43a65a2578a7ebd30ef29f85
Snok <chocowaco152@gmail.noreply.github.com>
2024-10-09 01:23:33 -0700

    Add grid size changer

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+function changeGridSize() {
+
+}
+
 function createBox() {
     const div = document.createElement("div");
     div.classList.add("box")
@@ -5,22 +9,39 @@ function createBox() {
         width: 20px; 
         height: 20px;
         box-sizing: border-box;
-\        border-style: solid;
+        border-style: solid;
         border-width: 2px;
         border-color: black;`
     );
     return div;
 }
 
-function createGrid() {
-    const grid = document.querySelector("#grid");
-    for (let i = 0; i < 16*16; i++) {
+function createGrid(size) {
+    const grid = document.querySelector(".grid");
+    grid.style.maxWidth = `calc(20px * ${size})`;
+    for (let i = 0; i < size*size; i++) {
         const box = createBox();
         grid.appendChild(box);
     }
 }
 
-createGrid();
+createGrid(16);
+
+const button = document.querySelector(".change-button");
+const input = document.querySelector(".change-input")
+const grid = document.querySelector(".grid");
+button.addEventListener("click", function() {
+    console.log("button pressed");
+    // TODO: value is blank
+    console.log(input.value);
+    if (input.value < 100 && input.value > 0) { 
+        while (grid.firstChild) {
+            grid.removeChild(grid.lastChild);
+        }
+        console.log("if statement passed");
+        createGrid(input.value);
+    }
+});
 
 const boxes = document.querySelectorAll(".box");
 for (let i = 0; i < boxes.length; i++) {
@@ -28,4 +49,3 @@ for (let i = 0; i < boxes.length; i++) {
         boxes[i].style.backgroundColor = "gray";
     });
 }
-
